72d6f36a8db9560a7dd51e38c54c39f401b7feb2
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-09-10 14:14:10 -0500

    CORS configuration

--- MinimalApi/MinimalApi/Program.cs
+++ MinimalApi/MinimalApi/Program.cs
@@ -1,7 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
+string CORS = "MyCORS";
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: CORS, builderCors => {
+        builderCors.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+            .AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors(CORS);
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.MapGet("/", () => "Hello World!");
+
+/*----- ENDPOINTS -----*/
 
 app.Run();
